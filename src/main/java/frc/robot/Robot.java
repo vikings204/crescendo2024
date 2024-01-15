115ccb174c64b29754631742da499179948a8e9c
neev <nevadex@gmail.com>
2024-01-15 01:34:20 -0500

    more cleanup of weird ctre stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,6 @@ import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.lib.config.CTREConfigs;
-//import com.pathplanner.lib.PathPlanner;
-//import com.pathplanner.lib.server.PathPlannerServer;
 
 
 /**
@@ -16,7 +13,6 @@ import frc.lib.config.CTREConfigs;
  * project.
  */
 public class Robot extends TimedRobot {
-    public static CTREConfigs ctreConfigs;
     private Command autonomousCommand;
     private Command teleopCommand;
     private RobotContainer robotContainer;
@@ -29,7 +25,6 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
-        ctreConfigs = new CTREConfigs();
 
         robotContainer = new RobotContainer();
         //CameraServer.startAutomaticCapture(); // use for USB camera
