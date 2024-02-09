74c65c96636cd3c8ee49e24f29b99b6dba67d267
neev <nevadex@gmail.com>
2024-02-09 16:26:54 -0500

    sensor is working

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,14 +1,13 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorSensorV3;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.util.CANSparkMaxUtil;
 import frc.lib.util.CANSparkMaxUtil.Usage;
@@ -111,7 +110,9 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void receive(boolean shoot) {
-        if (shoot) {
+        SmartDashboard.putNumber("DISTANCE", sensor1.getProximity());
+
+        if (shoot && sensor1.getProximity() < 600) {
             shooterMotor_1.set(-.05);
             shooterMotor_2.set(-.05);
             bumpMotor.set(-.05);
